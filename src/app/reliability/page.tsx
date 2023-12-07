24fd038ddcfd6bd937cc74c65fa9afdafacf24e1
nicolofabris <nicolo.fabriss@gmail.com>
2023-12-07 12:04:33 +0100

    home page + routes

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import reliabilityImg from "public/reliability.jpg";
+
+export default function ReliabilityPage() {
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="welding"
+      title="Super high reliability hosting"
+    />
+  );
+}
